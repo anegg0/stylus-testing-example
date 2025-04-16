63a907b37f6bf3df802772f90b5735aef7a510ed
Gaël Blanchemain <gael.blanchemain@protonmail.com>
2025-04-16 16:58:09 -0700

    fix: rename ref to project

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,5 @@ pub extern "C" fn main() {}
 
 #[cfg(feature = "export-abi")]
 fn main() {
-    stylus_cupcake_example::print_abi("MIT-OR-APACHE-2.0", "pragma solidity ^0.8.23;");
+    stylus_testing_example::print_abi("MIT-OR-APACHE-2.0", "pragma solidity ^0.8.23;");
 }
